0dcf155df96b8ffc36268e856800e27ebc52de10
Colin Kennedy <colinvfx@gmail.com>
2026-01-01 18:49:37 -0800

    feat(mapping): added [q / ]q / [l / ]l auto-wrap mappings

--- init.lua
+++ init.lua
@@ -6347,8 +6347,7 @@ do -- NOTE: Better git-diff support. It has git marks + "load the git diff as a 
     )
 end
 
-do  -- NOTE: Add an automated winbar title to the Quickfix window.
-
+do -- NOTE: Add an automated winbar title to the Quickfix window.
     ---@return string # The recommended Quickfix window title, if any is defined.
     function _P.get_quickfix_winbar_title()
         local info = vim.fn.getqflist({ title = 0 })
@@ -6370,4 +6369,38 @@ do  -- NOTE: Add an automated winbar title to the Quickfix window.
             vim.wo[window].winbar = "%{%v:lua.get_quickfix_winbar_title()%}"
         end,
     })
+end
+
+do -- NOTE: Make []q/[]l mappings auto-wrap. Seriously why are these not the default?
+    vim.keymap.set("n", "[q", function()
+        local success = pcall(vim.cmd.cprevious)
+
+        if not success then
+            vim.cmd.clast()
+        end
+    end, { desc = "Go to the previous Quickfix entry or wrap around to the end.", silent = true })
+
+    vim.keymap.set("n", "]q", function()
+        local success = pcall(vim.cmd.cnext)
+
+        if not success then
+            vim.cmd.cfirst()
+        end
+    end, { desc = "Go to the next Quickfix entry or wrap around to the start.", silent = true })
+
+    vim.keymap.set("n", "[l", function()
+        local success = pcall(vim.cmd.lprevious)
+
+        if not success then
+            vim.cmd.llast()
+        end
+    end, { desc = "Go to the previous Location List entry or wrap around to the end.", silent = true })
+
+    vim.keymap.set("n", "]l", function()
+        local success = pcall(vim.cmd.lnext)
+
+        if not success then
+            vim.cmd.lfirst()
+        end
+    end, { desc = "Go to the next Location List entry or wrap around to the start.", silent = true })
 end

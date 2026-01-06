dc2954a8123cb86e1856465f71f76316c72733a0
Colin Kennedy <colinvfx@gmail.com>
2026-01-05 19:48:26 -0800

    feat(formatter): trailing whitespace is removed automatically on-buffer-save

--- init.lua
+++ init.lua
@@ -2291,6 +2291,11 @@ function _P.setup_lsp_details(args)
         desc = "Find and [g]o to the [i]mplementation of some header / declaration.",
     })
 
+    vim.keymap.set("i", "<C-k>", function()
+        -- Reference: https://github.com/neovim/neovim/issues/37191
+        vim.lsp.buf.hover({ zindex = 300 })
+    end, { desc = "Show documentation for the current WORD under the cursor." })
+
     local identifier = args.data.client_id
     local client =
         assert(vim.lsp.get_client_by_id(identifier), string.format('Identifier "%s" has no LSP client.', identifier))
@@ -4025,43 +4030,112 @@ do -- NOTE: Visualize trailing whitespace
     vim.cmd([[match TrailingWhitespace /\s\+$/]])
 end
 
-do -- NOTE: Remove trailing whitespace from modified lines
-    -- TODO: In the future this should be modified to work in visual mode, too
-    ---@type table<integer, boolean>
-    local _MODIFIED_LINES = {}
+do -- NOTE: Remove trailing whitespace but only on lines you've modified.
+    local _GROUP = vim.api.nvim_create_augroup("my.formatter.strip_trailing_whitespace", { clear = true })
+    ---@type table<integer, table<integer, boolean>>
+    local _LINES = {}
 
-    vim.api.nvim_create_autocmd("InsertCharPre", {
+    --- Group `values` according to nearest neighbors.
+    ---
+    --- A group is "2 or more values that are contiguous". All groups have
+    --- exactly 2 elements. Any orphans will be in a group by themselves.
+    ---
+    ---@param values integer[]
+    ---    The values to group-up.
+    ---    e.g. `{1, 2, 3, 7, 9, 10, 11, 13, 14, 18, 19, 20}`
+    ---@return {[1]: integer, [2]: integer}[]
+    ---    All tuples of start-and-end-group pairs.
+    ---
+    function _P.get_contiguous_chunks(values)
+        ---@type integer[][]
+        local result = {}
+
+        if #values == 0 then
+            return result
+        end
+
+        local start = values[1]
+        local previous = values[1]
+
+        for i = 2, #values do
+            local current = values[i]
+
+            if current == previous + 1 then
+                previous = current
+            else
+                -- NOTE: Close the previous group.
+                if start == previous then
+                    table.insert(result, { start, start })
+                else
+                    table.insert(result, { start, previous })
+                end
+
+                start = current
+                previous = current
+            end
+        end
+
+        -- NOTE: Close the final group.
+        if start == previous then
+            table.insert(result, { start, start })
+        else
+            table.insert(result, { start, previous })
+        end
+
+        return result
+    end
+
+    --- Remove all trailing whitespaces for all changed lines in the current buffer.
+    function _P.strip_trailing_whitespaces()
+        local buffer = vim.api.nvim_get_current_buf()
+        local lines = vim.tbl_keys(_LINES[buffer])
+        table.sort(lines)
+
+        -- NOTE: The chunks are actually necessary but they do cut down on the
+        -- number of `vim.cmd` calls that we have to do later so we'll chunk anyway.
+        --
+        local line_chunks = _P.get_contiguous_chunks(lines)
+
+        -- NOTE: Save and restore the user's cursor, jumplist, etc. Strip the whitespace.
+        -- Then restore any state that we need to.
+        --
+        local view = vim.fn.winsaveview()
+        pcall(function()
+            for _, chunk in ipairs(line_chunks) do
+                local start, last = unpack(chunk)
+                vim.cmd(string.format("keeppatterns %s,%ss/\\s\\+$//e", start, last))
+            end
+        end)
+        vim.fn.winrestview(view)
+
+        _LINES[buffer] = {}
+    end
+
+    vim.api.nvim_create_user_command(
+        "StripTrailingWhitespaces",
+        _P.strip_trailing_whitespaces,
+        { nargs = 0, desc = "Remove all trailing whitespaces from all changed lines." }
+    )
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = _GROUP,
         callback = function()
-            local line = vim.api.nvim_win_get_cursor(0)[1]
-            _MODIFIED_LINES[line] = true
+            vim.api.nvim_buf_attach(0, false, {
+                on_lines = function(_, buffer, _, first_line, _, last_line)
+                    vim.schedule(function()
+                        for index = first_line + 1, last_line do
+                            _LINES[buffer] = _LINES[buffer] or {}
+                            _LINES[buffer][index] = true
+                        end
+                    end)
+                end,
+            })
         end,
-        pattern = "*",
     })
 
-    vim.api.nvim_create_autocmd("InsertLeave", {
-        callback = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-
-            ---@type integer[]
-            local lines = {}
-
-            for line in pairs(_MODIFIED_LINES) do
-                table.insert(lines, line)
-            end
-
-            table.sort(lines)
-
-            for _, line in ipairs(lines) do
-                local content = vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
-
-                if content then
-                    local cleaned = content:gsub("%s+$", "")
-                    vim.api.nvim_buf_set_lines(bufnr, line - 1, line, false, { cleaned })
-                end
-            end
-
-            _MODIFIED_LINES = {}
-        end,
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        group = _GROUP,
+        callback = _P.strip_trailing_whitespaces,
     })
 end
 
@@ -6062,7 +6136,9 @@ do -- NOTE: Add mksession support.
 end
 
 -- TODO: Once `SessionLoadPre` exists, use that instead of this
-pcall(function() _SESSION_MANAGER:sync_current_session() end)
+pcall(function()
+    _SESSION_MANAGER:sync_current_session()
+end)
 
 do -- NOTE: A really basic git command wrapper.
     vim.api.nvim_create_user_command("Git", function(opts)

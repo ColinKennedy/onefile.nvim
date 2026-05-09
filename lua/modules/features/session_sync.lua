3daec93ad704b931361c51a1403e205f3f3a7955
Colin Kennedy <colinvfx@gmail.com>
2026-05-09 09:43:07 -0700

    Added SessionLoadPre

--- lua/modules/features/session_sync.lua
+++ lua/modules/features/session_sync.lua
@@ -1,8 +1,11 @@
-local core_editor_setup = require("modules.features.core_editor_setup")
-
 --- Session sync
 
--- TODO: Once `SessionLoadPre` exists, use that instead of this
-pcall(function()
-    core_editor_setup._SESSION_MANAGER:sync_current_session()
-end)
+vim.api.nvim_create_autocmd("SessionLoadPre", {
+    group = vim.api.nvim_create_augroup("my.session_sync", { clear = true }),
+    callback = function()
+        -- NOTE: We use `pcall` because this method will error if there is no session to load
+        pcall(function()
+            require("modules.features.core_editor_setup")._SESSION_MANAGER:sync_current_session()
+        end)
+    end,
+})
